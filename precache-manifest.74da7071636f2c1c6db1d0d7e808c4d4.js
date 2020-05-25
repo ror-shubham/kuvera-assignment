e3d031940e5e50524cf983d0155d78b73898a8e2
shubh <ror.shubh@gmail.com>
2020-05-25 15:58:25 +0530

    Updates

---
+++ precache-manifest.74da7071636f2c1c6db1d0d7e808c4d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d08e2baa5f9857bf7da8c246db4e4c",
+    "url": "/kuvera-assignment/index.html"
+  },
+  {
+    "revision": "15fa584aa08d28f22e8b",
+    "url": "/kuvera-assignment/static/css/main.01384089.chunk.css"
+  },
+  {
+    "revision": "dcec9667a5770486fb7d",
+    "url": "/kuvera-assignment/static/js/2.7e93c443.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/kuvera-assignment/static/js/2.7e93c443.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15fa584aa08d28f22e8b",
+    "url": "/kuvera-assignment/static/js/main.46ae03bd.chunk.js"
+  },
+  {
+    "revision": "ab47358c436463c0ed40",
+    "url": "/kuvera-assignment/static/js/runtime-main.1ad17182.js"
+  }
+]);
